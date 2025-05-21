9e667a49b0906b560df03939a6ee242fd980e475
xlab992 <demon_raz92@hotmail.it>
2025-05-21 10:54:16 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,8 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://xlab992-bb.hf.space"     # non mettere lo / finale al link
+MFPPSW = "gf4"
 
 # Costanti
 M3U8_OUTPUT_FILE = "247ita.m3u8"
